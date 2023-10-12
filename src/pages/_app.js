9d1739a836a5bc85b4dd39793c8adbbfa5ee492e
aulth <mohdusman.you@gmail.com>
2023-10-12 21:39:51 +0530

    adsense connneced

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import AppContext from 'AppContext'
 import { useState } from 'react';
 import NextNProgress from 'nextjs-progressbar';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 export default function App({ Component, pageProps }) {
   const [projectsTitle, setProjectsTitle] = useState('All Projects');
   const router = useRouter();
@@ -10,12 +11,12 @@ export default function App({ Component, pageProps }) {
     openDrawer();
     document.querySelectorAll('.project').forEach(item => { item.classList.remove('flex'); item.classList.add('hidden') });
     document.querySelectorAll(`.${category}`).forEach(item => { item.classList.remove('hidden'); item.classList.add('flex') });
-    setProjectsTitle(category+" Projects");
+    setProjectsTitle(category + " Projects");
   }
   const openDrawer = () => {
     if (typeof window !== 'undefined') {
       setProjectsTitle("All Projects");
-      document.querySelectorAll('.project').forEach((item=>{item.classList.remove('hidden');item.classList.add('flex')}))
+      document.querySelectorAll('.project').forEach((item => { item.classList.remove('hidden'); item.classList.add('flex') }))
       document.querySelector("#all-projects").classList.remove('-left-full');
       document.querySelector("#all-projects").classList.add('left-0');
     }
@@ -26,12 +27,16 @@ export default function App({ Component, pageProps }) {
       document.querySelector("#all-projects").classList.add('-left-full');
     }
   }
-  return <AppContext.Provider value={{filterProjects:filterProjects,
-  projectsTitle:projectsTitle,
-  openDrawer:openDrawer,
-  closeDrawer:closeDrawer
+  return <AppContext.Provider value={{
+    filterProjects: filterProjects,
+    projectsTitle: projectsTitle,
+    openDrawer: openDrawer,
+    closeDrawer: closeDrawer
   }}>
-      <NextNProgress color={router.pathname=='/'?'#73F8AB':'#2D2F33'} height={router.pathname=='/'?2:1} />
+    <Head>
+      <meta name="google-adsense-account" content="ca-pub-3323291309040065" />
+    </Head>
+    <NextNProgress color={router.pathname == '/' ? '#73F8AB' : '#2D2F33'} height={router.pathname == '/' ? 2 : 1} />
     <Component {...pageProps} />
   </AppContext.Provider>
 }
